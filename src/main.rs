29570ff4360fd92a6aa7c7f69de049f38bb84da0
Justas Lisajus <lisajusjustas@gmail.com>
2024-03-07 22:20:11 +0200

    feat: spawn food and food collision

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use window::{CustomPlugin, WINDOW_HEIGHT, WINDOW_WIDTH};
 use bevy::input::ButtonInput;
 use bevy_framepace::Limiter;
 use helper::MathHelper;
+use rand::Rng;
 
 const SQUARE_THICKNESS : f32 = 25.0;
 
@@ -20,6 +21,24 @@ enum Direction {
 #[derive(Component)]
 struct Snake{
     is_dead: bool
+}
+
+#[derive(Component)]
+struct Food;
+
+fn get_food_position(snake_position : Vec3) -> (f32, f32) {
+    let spawnable_area_width = WINDOW_WIDTH - SQUARE_THICKNESS * 2.0;
+    let spawnable_area_height = WINDOW_HEIGHT - SQUARE_THICKNESS * 2.0;
+
+    let mut x : f32 = 0.0;
+    let mut y : f32 = 0.0;
+
+    while (x == snake_position.x || x == 0.0) && (y == snake_position.y || y == 0.0){
+        let mut rng = rand::thread_rng();
+        x = rng.gen_range(-spawnable_area_width / 2.0..spawnable_area_width / 2.0);
+        y = rng.gen_range(-spawnable_area_height / 2.0..spawnable_area_height / 2.0);
+    }
+    (x, y)
 }
 
 fn setup(mut commands: Commands, mut settings: ResMut<bevy_framepace::FramepaceSettings>) {
@@ -104,17 +123,39 @@ fn move_snake(mut query: Query<(&Direction, &mut Transform), With<Snake>>){
     }
 }
 
-fn check_border_collision(mut query : Query<(&Transform, &mut Snake)>){
-    for(snake_transform, mut snake) in query.iter_mut(){
-        let snake_position = snake_transform.translation;
+fn handle_collisions( mut snake_param: ParamSet<(
+    Query<(Entity, &Transform, &mut Snake)>,
+    Query<(Entity, &mut Transform), With<Food>>,
+)>){
+    let mut snake_query = snake_param.p0();
+    let mut snake_position : Vec3 = Vec3::new(0.0, 0.0, 0.0);
+    for(_snake_entity, snake_transform, mut snake) in snake_query.iter_mut(){
+        snake_position = snake_transform.translation;
         println!("Snake Y:{:?}", snake_position.y);
         println!("Snake X:{:?}", snake_position.x);
+
+        //Check border collision
         let hit_vertical_border = ((snake_position.y + SQUARE_THICKNESS/ 2.0) >= MathHelper::round(WINDOW_HEIGHT / 2.0 - SQUARE_THICKNESS, 1))
                                     || ((snake_position.y - SQUARE_THICKNESS / 2.0) <= MathHelper::round(-(WINDOW_HEIGHT / 2.0 - SQUARE_THICKNESS), 1));
         let hit_horizontal_border = ((snake_position.x + SQUARE_THICKNESS / 2.0) >= MathHelper::round(WINDOW_WIDTH / 2.0 - SQUARE_THICKNESS, 1))
                                         || ((snake_position.x - SQUARE_THICKNESS / 2.0) <= MathHelper::round(-(WINDOW_WIDTH / 2.0 - SQUARE_THICKNESS), 1));
 
         snake.is_dead = hit_horizontal_border || hit_vertical_border;
+        if snake.is_dead {
+            println!("Hit border");
+            return;
+        }
+    }
+    let mut food_query = snake_param.p1();
+    for (_food_entity, mut food_transform,) in food_query.iter_mut(){
+        if snake_position.distance(food_transform.translation) < SQUARE_THICKNESS {
+
+            //Change food position
+            let position = get_food_position(snake_position);
+            food_transform.translation.x = position.0;
+            food_transform.translation.y = position.1;
+            //Handle snake growth;
+        }
     }
 }
 
@@ -126,15 +167,65 @@ fn handle_snake_death(mut query: Query<&Snake>, mut exit: EventWriter<AppExit>){
     }
 }
 
+fn spawn_food(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>, mut snake_query : Query<&Transform, With<Snake>>){
+    let mut snake_position: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+    for transform in snake_query.iter_mut(){
+        snake_position = transform.translation;
+    }
+   
+    let position = get_food_position(snake_position);
+    
+    let food_mesh = Mesh2dHandle(meshes.add(Rectangle::new(SQUARE_THICKNESS, SQUARE_THICKNESS)));
+
+    commands.spawn((Food, MaterialMesh2dBundle{
+        mesh: food_mesh,
+        material: materials.add(Color::rgb(1.0, 1.0, 1.0)),
+        transform: Transform::from_xyz(position.0, position.1, 0.0),
+        ..default()
+    }));
+}
+
+// fn spawn_food(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>, mut snake_query : Query<&Transform, With<Snake>>, mut food_query : Query<&Food>){
+//     let mut snake_position: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+//     for transform in snake_query.iter_mut(){
+//         snake_position = transform.translation;
+//     }
+//     for food in food_query.iter_mut(){
+//         if !food.is_eaten{
+//             let spawnable_area_width = WINDOW_WIDTH - SQUARE_THICKNESS * 2.0;
+//             let spawnable_area_height = WINDOW_HEIGHT - SQUARE_THICKNESS * 2.0;
+    
+//             let mut x : f32 = 0.0;
+//             let mut y : f32 = 0.0;
+    
+//             while (x == snake_position.x || x == 0.0) && (y == snake_position.y || y == 0.0){
+//                 let mut rng = rand::thread_rng();
+//                 x = rng.gen_range(-spawnable_area_width / 2.0..spawnable_area_width / 2.0);
+//                 y = rng.gen_range(-spawnable_area_height / 2.0..spawnable_area_height / 2.0);
+//             }
+            
+//             let food_mesh = Mesh2dHandle(meshes.add(Rectangle::new(SQUARE_THICKNESS, SQUARE_THICKNESS)));
+    
+//             commands.spawn((Food{is_eaten: false}, MaterialMesh2dBundle{
+//                 mesh: food_mesh,
+//                 material: materials.add(Color::rgb(1.0, 1.0, 1.0)),
+//                 transform: Transform::from_xyz(x, y, 0.0),
+//                 ..default()
+//             }));
+//         }
+//     }
+// }
+
 fn main() {
     App::new()
           .add_plugins((CustomPlugin, bevy_framepace::FramepacePlugin))
           .add_systems(Startup, setup)
           .add_systems(Startup, generate_borders)
-          .add_systems(Startup, spawn_snake)
-          .add_systems(Update, handle_input)
-          .add_systems(Update, move_snake)
-          .add_systems(Update, check_border_collision)
-          .add_systems(Update, handle_snake_death)
+          .add_systems(Startup, (spawn_snake, spawn_food))
+          .add_systems(Update, (handle_collisions,
+                                                handle_snake_death,
+                                                handle_input,
+                                                move_snake,
+                                                ))
           .run();
 }
